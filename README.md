111f257e92d15d875d84b568d957ad58058cb316
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-07 13:40:33 -0300

    method with generic

--- README.md
+++ README.md
@@ -5,3 +5,29 @@
 List<String> list = new Array<>()
 ```
 - garante type safety e maior performace no lugar de colocar os parametros como Object
+- Metodo com geric
+- Comparable<T> -> tipo comparavel T
+- Comparable<? super T> -> tipo comparavel T ou super T (acima de T)
+```java
+
+public static <T extends Comparable<? super T>> T max(List<T> list) {
+  if (list.isEmpty()) {
+    throw new IllegalStateException("List can't be empty");
+  }
+  T max = list.get(0);
+  for (T item: list) {
+    if (item.compareTo(max) > 0) {
+      max = item;
+    }
+  }
+  return max;
+}  
+```
+# Dicas gerais
+- list.compareTo
+  - Compara se o primeiro é maior que segundo (do parametro)
+  - return 0 se for igual item
+  - return 1 se for maior
+  - return -1 se for menor
+
+
--- src/App.java
+++ src/App.java
@@ -1,25 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import generics.entities.Product;
+import generics.service.CalculationService;
 import generics.service.PrintService;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        List<Product> list = new ArrayList<>();
+        String path = "../in.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = reader.readLine();
+            }
 
-        PrintService<Integer> ps = new PrintService<>();
-
-        System.out.print("How many values? ");
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            Integer value = sc.nextInt();
-            ps.addValue(value);
+            Product x = CalculationService.max(list);
+            System.out.println("Max");
+            System.out.println(x);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
-
-        ps.print();
-        Integer x = ps.first();
-        System.out.println("First: " + x);
-
-        sc.close();
     }
 }
